e445b6aa6813503bd9ec75d301d132f4acc13e15
Ioana Onofrei <jeanneomarie01@gmail.com>
2022-10-23 18:41:10 +0300

    Update BitOperations.java

--- src/BitOperations.java
+++ src/BitOperations.java
@@ -6,14 +6,14 @@ public class BitOperations {
         int inaltime = 168; //8 bits
         short info_impachetata; // 16 bits
 
-        // împachetare
-        info_impachetata = (short) (varsta);
+        // packing
+        info_impachetata = (short) (varsta); // casting
         info_impachetata = (short) (info_impachetata << 1);
         info_impachetata = (short) (info_impachetata | sex);
         info_impachetata = (short) (info_impachetata << 8);
         info_impachetata = (short) (info_impachetata | inaltime);
 
-        // despachetare
+        // unpacking
         // X & 1 = X
         int inaltime2 = info_impachetata & 0xff;
         int sex2 = (info_impachetata >>> 8) & 1;
